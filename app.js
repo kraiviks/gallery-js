356098427d7d8450794b2c3cf57b73ad1d167bbd
Kraiviks <kraiviks@gmail.com>
2022-01-09 17:29:11 +0200

    added change card by arrow button

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 window.addEventListener("DOMContentLoaded", () => {
 	const slides = document.querySelectorAll(".item");
+	let initialSlide = 2;
+	
+	slides[initialSlide].classList.add("active");
 
 	slides.forEach((slide) => {
 		slide.addEventListener("click", () => {
@@ -12,4 +15,28 @@ window.addEventListener("DOMContentLoaded", () => {
 			slide.classList.add("active");
 		});
 	});
+
+	document.addEventListener("keydown", (e) => {
+		if (e.key === "ArrowLeft") {
+			initialSlide--;
+			if (initialSlide < 0) {
+				initialSlide = slides.length - 1;
+			}
+
+			changeSlide();
+		} else if (e.key === "ArrowRight") {
+			initialSlide++;
+			if (initialSlide === slides.length) {
+				initialSlide = 0;
+			}
+			changeSlide();
+		}
+	});
+
+	function changeSlide() {
+		slides.forEach((element) => {
+			element.classList.remove("active");
+		});
+		slides[initialSlide].classList.add("active");
+	}
 });
